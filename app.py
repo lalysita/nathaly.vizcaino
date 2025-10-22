d40276c5a6f0eb5a0b5be64e8154928cd1ce8e4d
lalysita <cnb.vizcaino@yavirac.edu.ec>
2025-10-22 15:34:21 -0500

    arreglo

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def sumar(a, b):
 
 @app.route('/')
 def hola_mundo():
-    return 'Adiós, Mundo!', 200 # Este cambio hará que la prueba falle
+   return 'Hola, Mundo!', 200 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
