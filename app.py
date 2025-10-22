3573395d141616caa4cf3dfd1578277417636e22
lalysita <cnb.vizcaino@yavirac.edu.ec>
2025-10-22 15:33:07 -0500

    error

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def sumar(a, b):
 
 @app.route('/')
 def hola_mundo():
-    return 'Hola, Mundo!', 200
+    return 'Adiós, Mundo!', 200 # Este cambio hará que la prueba falle
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
