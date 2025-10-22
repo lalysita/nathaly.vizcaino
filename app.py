9e992808f30272c8549c24f994e294dca0642b61
lalysita <cnb.vizcaino@yavirac.edu.ec>
2025-10-22 15:24:19 -0500

    examen

---
+++ app.py
@@ -0,0 +1,15 @@
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+# Función para prueba unitaria
+def sumar(a, b):
+    return a + b
+
+@app.route('/')
+def hola_mundo():
+    return 'Hola, Mundo!', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
